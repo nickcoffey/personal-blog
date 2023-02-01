c359fd0edc364c06b4f1fd2bf7344abfa90aa4ca
Nick Coffey <nicholasjcoffey@gmail.com>
2023-01-31 23:13:58 -0600

    moved font code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export default function Home() {
   return (
@@ -15,7 +12,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className={`${inter.variable} font-sans`}>
+      <main>
         <h1 className='text-5xl'>My Blog</h1>
       </main>
     </>
